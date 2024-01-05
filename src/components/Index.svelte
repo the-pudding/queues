a153dc8889c78e4866a2bec1f29e73040f7a79fc
Michelle McGhee <michmac202@gmail.com>
2024-01-05 10:51:51 -0800

    getting started with rive

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,60 @@
 <script>
-	import { getContext } from "svelte";
-	import Demo from "$components/demo/Demo.svelte";
-	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
+	import { onMount } from "svelte";
+	import { Rive } from "@rive-app/canvas";
+	import Scrolly from "$components/helpers/Scrolly.svelte";
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
+	let canvasEl;
+	let value;
+
+	onMount(() => {
+		const r = new Rive({
+			src: "https://cdn.rive.app/animations/vehicles.riv",
+			canvas: canvasEl,
+			autoplay: true,
+			stateMachines: "bumpy",
+			onLoad: () => {
+				r.resizeDrawingSurfaceToCanvas();
+			}
+		});
+	});
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+<section id="scrolly">
+	<div class="sticky">
+		<h2>step <span>{value}</span></h2>
+		<canvas bind:this={canvasEl} width="500" height="500"></canvas>
+	</div>
+	<div class="spacer" />
+	<Scrolly bind:value>
+		{#each [0, 1, 2, 3, 4] as text, i}
+			{@const active = value === i}
+			<div class="step" class:active>
+				<p>{text}</p>
+			</div>
+		{/each}
+	</Scrolly>
+	<div class="spacer" />
+</section>
+
+<style>
+	.sticky {
+		position: sticky;
+		top: 2rem;
+		width: 60%;
+		height: calc(100vh - 2rem);
+	}
+
+	.spacer {
+		height: 75vh;
+	}
+
+	.step {
+		height: 80vh;
+		background: var(--color-gray-100);
+		text-align: center;
+	}
+
+	.step p {
+		padding: 1rem;
+	}
+</style>
